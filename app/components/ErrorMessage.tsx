35d04e41db7d43f149673bb3a671d3d3ac50eafc
PhilipOyelegbin <philipoyelegbin@gmail.com>
2024-05-26 08:47:43 +0100

    dev area

--- app/components/ErrorMessage.tsx
+++
@@ -1,9 +0,0 @@
-import React, { PropsWithChildren } from 'react'
-
-function ErrorMessage({children}: PropsWithChildren) {
-    if(!children) return null;
-
-  return <p className='text-red-500'>{children}</p>
-}
-
-export default ErrorMessage
--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <article>
-      <h3>A bug tracking application...</h3>
+      Dashboard
     </article>
   );
 }
